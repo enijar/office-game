423f5b2ae3d2dcccec0802ce6f305953c295208f
Enijar <TheEnijar@gmail.com>
2024-04-24 23:11:33 +0100

    chore: add obsticles to level

--- src/game/level.tsx
+++ src/game/level.tsx
@@ -13,15 +13,31 @@ export default function Level() {
   texture.anisotropy = useThree((state) => state.gl.capabilities.getMaxAnisotropy());
 
   return (
-    <RigidBody type="fixed" colliders="trimesh">
+    <>
+      {/* floor */}
       <mesh rotation={[THREE.MathUtils.degToRad(-90), 0, 0]}>
         <planeGeometry args={[config.bounds.x, config.bounds.z]} />
         <meshBasicMaterial map={texture} color="#555555" />
       </mesh>
-      <mesh position={[0, config.bounds.y / 2, 0]}>
-        <boxGeometry args={[config.bounds.x, config.bounds.y, config.bounds.z]} />
-        <meshBasicMaterial color="#555555" side={THREE.BackSide} transparent opacity={0} />
-      </mesh>
-    </RigidBody>
+      <RigidBody type="fixed" colliders="trimesh">
+        {/* bounds */}
+        <mesh position={[0, config.bounds.y / 2, 0]}>
+          <boxGeometry args={[config.bounds.x, config.bounds.y, config.bounds.z]} />
+          <meshBasicMaterial color="#555555" side={THREE.BackSide} transparent opacity={0} />
+        </mesh>
+        <mesh position={[-1, 0.05, 5]}>
+          <boxGeometry args={[1, 0.1, 1]} />
+          <meshBasicMaterial color="#666666" />
+        </mesh>
+        <mesh position={[-1, 0.2, 5.5]}>
+          <boxGeometry args={[1, 0.1, 1]} />
+          <meshBasicMaterial color="#666666" />
+        </mesh>
+        <mesh position={[-1, 0.35, 6]}>
+          <boxGeometry args={[1, 0.1, 1]} />
+          <meshBasicMaterial color="#666666" />
+        </mesh>
+      </RigidBody>
+    </>
   );
 }
