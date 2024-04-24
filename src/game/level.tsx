5a59353d91485511291e1ccd67b3d492f5839d5c
Enijar <TheEnijar@gmail.com>
2024-04-24 22:48:01 +0100

    chore: remove unused code in level/player

--- src/game/level.tsx
+++ src/game/level.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import * as THREE from "three";
 import { RigidBody } from "@react-three/rapier";
-import { useGame } from "ecctrl";
 import config from "@/game/config.ts";
 
 export default function Level() {
-  const setMoveToPoint = useGame((state) => state.setMoveToPoint);
-  const dateRef = React.useRef(0);
-
   return (
-    <>
-      <RigidBody type="fixed" colliders="trimesh">
-        <mesh rotation={[THREE.MathUtils.degToRad(-90), 0, 0]}>
-          <planeGeometry args={[config.bounds.x, config.bounds.z]} />
-          <meshBasicMaterial color="#555555" />
-        </mesh>
-        {/*<mesh position={[0, config.bounds.y / 2, 0]}>*/}
-        {/*  <boxGeometry args={[config.bounds.x, config.bounds.y, config.bounds.z]} />*/}
-        {/*  <meshBasicMaterial color="#555555" side={THREE.BackSide} transparent opacity={0} />*/}
-        {/*</mesh>*/}
-      </RigidBody>
-    </>
+    <RigidBody type="fixed" colliders="trimesh">
+      <mesh rotation={[THREE.MathUtils.degToRad(-90), 0, 0]}>
+        <planeGeometry args={[config.bounds.x, config.bounds.z]} />
+        <meshBasicMaterial color="#555555" />
+      </mesh>
+      <mesh position={[0, config.bounds.y / 2, 0]}>
+        <boxGeometry args={[config.bounds.x, config.bounds.y, config.bounds.z]} />
+        <meshBasicMaterial color="#555555" side={THREE.BackSide} transparent opacity={0} />
+      </mesh>
+    </RigidBody>
   );
 }
--- src/game/player.tsx
+++ src/game/player.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import * as THREE from "three";
 import config from "@/game/config.ts";
 
 export default function Player() {
-  const groupRef = React.useRef<THREE.Group>(null);
-
   return (
-    <group ref={groupRef}>
-      <mesh>
-        <capsuleGeometry args={[config.player.radius, config.player.length]} />
-        <meshBasicMaterial color="crimson" />
-      </mesh>
-    </group>
+    <mesh>
+      <capsuleGeometry args={[config.player.radius, config.player.length]} />
+      <meshBasicMaterial color="crimson" />
+    </mesh>
   );
 }
